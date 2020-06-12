671e24ba8edf7d44b3774e7ca60b1308d9179dce
Abhishek <abhishek.ghoshal.ncrts@gmail.com>
2020-06-12 11:18:15 +0530

    Screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -12,6 +12,14 @@ class _AppState extends State<App> {
     return MaterialApp(
       title: "Log me in",
       home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.lightGreen,
+          title: Center(
+            child: Text(
+              "Wanna Logged in",
+                style: TextStyle(fontSize: 20,color: Colors.black54)
+            ),
+          ),
+        ),
         backgroundColor: Colors.blueGrey,
         body: LogInScreen(),
       ),
